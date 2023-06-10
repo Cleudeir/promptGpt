7a0003bd8b09a60f1b789079dd2a910334413357
Cleudeir <cleudeirsilva@gmail.com>
2023-06-10 15:13:23 -0300

    fixBug

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-  chrome.scripting.registerContentScripts([{
-    id : "test",
-    matches : [ "https://*.nytimes.com/*" ],
-    runAt : "document_start",
-    js : [ "contentScript.js" ],
-  }]);
- 
-
-  
