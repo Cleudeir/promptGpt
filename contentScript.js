4a6569edc7eebbcba9066723d435b021cdb5091d
Cleudeir <cleudeirsilva@gmail.com>
2023-06-10 02:56:21 -0300

    fixBug

--- contentScript.js
+++ contentScript.js
@@ -8,9 +8,9 @@ async function contentScript() {
     }
     const remember = await chrome.storage.sync.get("prompt")
     body.innerHTML = `
-    <div class="p-8 h-64 w-full gap-4 flex justify-center items-start flex-col">
-        <textarea id="prompt" type="text" class="w-full border border-gray-300 rounded-md px-4 py-2 text-black">${remember.prompt}</textarea>
-        <button id="buttonClick" class="w-full bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+    <div class="p-8 h-full gap-2 w-full flex justify-center items-start flex-col">
+        <textarea id="prompt" type="text" class="h-96 w-full border border-gray-300 rounded-md px-4 py-2 text-black">${remember.prompt}</textarea>
+        <button id="buttonClick" class="w-full bg-green-500 hover:bg-green-600 text-white font-bold my-2 py-2 px-4 rounded">
             Click
         </button>
     </div>
@@ -20,11 +20,11 @@ async function contentScript() {
         const textArea = document.querySelectorAll("textarea")[1]
         const prompt = document.querySelector("#prompt")
         await chrome.storage.sync.set({ prompt: prompt.value })
-        textArea.value = prompt.value + "\ncode: "
+        textArea.value = prompt.value + "\n: "
         textArea.focus()
     }
 }
 
-setInterval(contentScript, 2000)
+setInterval(contentScript, 3000)
 
 
